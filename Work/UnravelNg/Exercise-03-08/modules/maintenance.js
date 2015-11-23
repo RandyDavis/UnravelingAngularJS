53557511b5f0156ef8394a6f45dced7e1d18c3db
Randy Davis <randy_davis@outlook.com>
2015-11-22 22:51:30 -0600

    refactor app to use modules

---
+++ Work/UnravelNg/Exercise-03-08/modules/maintenance.js
@@ -0,0 +1,13 @@
+angular.module('maintenance', ['core'])
+    .controller('locationsCtrl', LocationsCtrl)
+    .controller('sitesCtrl', SitesCtrl)
+    .controller('typesCtrl', TypesCtrl);
+
+    function LocationsCtrl (currentSpot) {
+    }
+
+    function SitesCtrl (currentSpot) {
+    }
+
+    function TypesCtrl () {
+    }
